56246ccb89045514f850f672cfb91536b7d69069
KangHyi Lee <corea22222@gmail.com>
2023-03-12 21:11:22 +0900

    feat: Modify Eslint Rules
    prettier 사용 안함 규칙 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,6 +45,7 @@ module.exports = {
 		"no-unused-vars": "off", // 미사용 변수 경고 처리 off 
 		"react/prop-types": "off", // PropType 타입 검사 off (TypeScript 를 사용하지 않기 때문에 필요 없음)
 		"no-undef": "off", // 선언하지 않은 변수의 사용을 금지 off
-		"react/react-in-jsx-scope": "off" // 항상 React 를 import 해야 하는 옵션 off
+		"react/react-in-jsx-scope": "off", // 항상 React 를 import 해야 하는 옵션 off
+		"prettier/prettier": 0, // prettier 사용하지 않음
 	}
 };
