872172b510f2c6deb9f36ba0cbd6a613ea9aa68e
KangHyi Lee <corea22222@gmail.com>
2023-06-11 20:59:32 +0900

    hotfix: Change header png to svg

--- src/components/tutorials/TutorialHeader.js
+++ src/components/tutorials/TutorialHeader.js
@@ -1,15 +1,16 @@
 import { useEffect } from 'react';
-import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { View, TouchableOpacity, Image } from 'react-native';
 import { Step } from '../../utils/tutorials/Step';
 import {useSelector, useDispatch} from 'react-redux';
 import { 
     setStep,
 } from '../../redux/reducerSlices/tutorialSlice';
 
-import RightBlackButtonImg from '../../assets/images/TutorialHeaderRightBlackButton.png';
-import LeftBlackButtonImg from '../../assets/images/TutorialHeaderLeftBlackButton.png';
-import RightWhiteButtonImg from '../../assets/images/TutorialHeaderRightWhiteButton.png';
-import LeftWhiteButtonImg from '../../assets/images/TutorialHeaderLeftWhiteButton.png';
+import { PretendardedText } from '../CustomComponent/PretendardedText';
+
+import LightBackIcon from '../../assets/icons/light/backIcon.svg';
+import DarkBackIcon from '../../assets/icons/dark/backIcon.svg';
+import LightNextIcon from '../../assets/icons/light/nextIcon.svg';
 
 export const TutorialHeader = (props) => {
     const dispatch = useDispatch();
@@ -57,11 +58,13 @@ export const TutorialHeader = (props) => {
 
     return (
     <View style={{flex: 1, margin:15, flexDirection:"row", justifyContent:"space-between"}}>
-        <TouchableOpacity onPress={clickLeftButton} style={{opacity:enableHeaderLeftBtn ? 1 : 0}} disabled={!enableHeaderLeftBtn}>
-            <Image source={enableBackgroundImg ? LeftWhiteButtonImg : LeftBlackButtonImg} />
+        <TouchableOpacity onPress={clickLeftButton} style={{opacity:enableHeaderLeftBtn ? 1 : 0, flexDirection:"row", alignItems:'center'}} disabled={!enableHeaderLeftBtn}>
+            {enableBackgroundImg ? <LightBackIcon /> : <DarkBackIcon/> }
+            <PretendardedText style={{color:enableBackgroundImg ? 'white' : 'black', marginLeft:4, fontSize:14}}>이전으로</PretendardedText>
         </TouchableOpacity>
-        <TouchableOpacity onPress={clickRightButton} style={{opacity:enableHeaderRightBtn ? 1 : 0}} disabled={!enableHeaderRightBtn}>
-            <Image source={enableBackgroundImg ? RightWhiteButtonImg : RightBlackButtonImg} />
+        <TouchableOpacity onPress={clickRightButton} style={{opacity:enableHeaderRightBtn ? 1 : 0, flexDirection:"row", alignItems:'center'}} disabled={!enableHeaderRightBtn}>
+            <PretendardedText style={{color:enableBackgroundImg ? 'white' : 'black', marginRight:4, fontSize:14}}>다음으로</PretendardedText>
+            <LightNextIcon/>
         </TouchableOpacity>
     </View>
     )
